38b5789b2627be149f112fae979a4bcae949a6b4
Darren <darren@darrenwhitlen.com>
2016-12-29 12:07:48 +0000

    Runtime string replacements for config.js values

--- src/libs/ConfigLoader.js
+++ src/libs/ConfigLoader.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import _ from 'lodash';
 
 export default class ConfigLoader {
     constructor() {
@@ -9,12 +10,27 @@ export default class ConfigLoader {
         return new Promise((resolve, reject) => {
             $.getJSON(configUrl)
             .done(data => {
-                this.config = data;
+                this.config = Object.create(null);
+                _.each(data, (val, key) => {
+                    this.config[key] = this.insertReplacements(val);
+                });
+
                 resolve(this.config);
             })
             .fail(() => {
                 reject();
             });
         });
+    }
+
+    insertReplacements(input) {
+        let out = input;
+        out = out.replace('{{hostname}}', window.location.hostname);
+        out = out.replace('{{host}}', window.location.host);
+        out = out.replace('{{port}}', window.location.port || 80);
+        out = out.replace('{{hash}}', (window.location.hash || '').substr(1));
+        out = out.replace('{{query}}', (window.location.search || '').substr(1));
+        out = out.replace('{{referrer}}', window.document.referrer);
+        return out;
     }
 }
