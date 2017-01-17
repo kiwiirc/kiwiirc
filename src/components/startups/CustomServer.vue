48760b03849da35f68edcf3ef2d59458b2242f00
Darren <darren@darrenwhitlen.com>
2017-01-17 21:20:49 +0000

    Parse connection strings from the URL fragment, supporting multiple connection strings

--- src/components/startups/CustomServer.vue
+++ src/components/startups/CustomServer.vue
@@ -102,13 +102,98 @@ export default {
     created: async function created() {
         let persist = new StatePersistence('kiwi-state', state, Storage, logger);
         await persist.loadStateIfExists();
-        persist.watchStateForChanges();
+
+        let saveThisSessionsState = false;
 
         // If we have networks from a previous state, launch directly into it
         if (state.networks.length > 0) {
             let network = state.networks[0];
             state.setActiveBuffer(network.id, network.serverBuffer().name);
+            saveThisSessionsState = true;
             this.$emit('start');
+        } else if (window.location.hash.substr(1)) {
+            let fragment = window.location.hash.substr(1);
+            // [ircs?://]irc.network.net:[+]6667/channel;
+            // Parse connection string such as this ^ into an object. Multiple connections
+            // may be given, separated by ;
+            let reg = /(?:(ircs?):\/\/)?([a-z.]+)(?::(?:(\+)?([0-9]+)))?(?:\/([^?]*))?(?:\?(.*))?/;
+            let connections = [];
+            fragment.split(';').forEach(connectionString => {
+                if (!connectionString) {
+                    return;
+                }
+
+                let m = connectionString.match(reg);
+
+                if (!m) {
+                    return;
+                }
+
+                let tls = m[1] === 'ircs' || !!m[3];
+                let params = Object.create(null);
+                (m[6] || '').split('&').forEach(p => {
+                    let parts = p.split('=');
+                    if (parts.length === 2) {
+                        params[parts[0].toLowerCase()] = parts[1];
+                    }
+                });
+
+                connections.push({
+                    tls: tls,
+                    server: m[2],
+                    port: parseInt(m[4] || (tls ? 6697 : 6667), 10),
+                    channel: m[5] || params.channel || '',
+                    nick: params.nick || '',
+                });
+            });
+
+            // If more than 1 connection string is given, skip the connection screen
+            // and add them all right away.
+            if (connections.length === 0) {
+                saveThisSessionsState = true;
+            } else if (connections.length === 1) {
+                saveThisSessionsState = false;
+
+                let con = connections[0];
+                this.server = con.server + ':' + con.port;
+                this.tls = con.tls;
+                this.nick = con.nick;
+            } else if (connections.length > 1) {
+                saveThisSessionsState = false;
+
+                connections.forEach((con, idx) => {
+                    let net = state.addNetwork(con.server, con.nick, {
+                        server: con.server,
+                        port: con.port,
+                        tls: con.tls,
+                        password: con.password || '',
+                    });
+
+                    if (con.channel) {
+                        con.channel.split(',').forEach(_channelName => {
+                            let hasPrefix = _channelName[0] === '#' ||
+                                _channelName[0] === '&';
+
+                            let channelName = hasPrefix ?
+                                _channelName :
+                                '#' + _channelName;
+
+                            state.addBuffer(net.id, channelName).joined = true;
+                        });
+                    }
+
+                    // Set the first server buffer active
+                    if (idx === 0) {
+                        state.setActiveBuffer(net.id, net.serverBuffer().name);
+                    }
+                });
+
+                this.$emit('start');
+            }
+        }
+
+        if (saveThisSessionsState) {
+            persist.watchStateForChanges();
         }
     },
 };
