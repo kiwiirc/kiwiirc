f0a3bbcfce34503414959d2d6734c7f89c175b8e
Darren <darren@darrenwhitlen.com>
2017-06-23 18:25:31 +0100

    Support a custom javascript function to provide the config on startup

--- src/main.js
+++ src/main.js
@@ -37,12 +37,20 @@ let configObj = null;
  * Finding the config file
  * In order, looks in the following places:
  *   1. If a 'config' argument is in the query string, static/config_THEVALUE_.json
- *   2. If a <meta name="kiwiconfig" content=""> is found, then the content becomes the config URL.
- *   3. If a <script name="kiwiconfig" type="application/json"></script> is found, then the content
+ *   2. If a window.kiwiConfig function exists, use it's return value as the config object.
+ *   3. If a <meta name="kiwiconfig" content=""> is found, then the content becomes the config URL.
+ *   4. If a <script name="kiwiconfig" type="application/json"></script> is found, then the content
  *      becomes the config JSON without making another web request.
  */
 if (getQueryVariable('config')) {
     configFile = 'static/config_' + getQueryVariable('config') + '.json';
+} else if (typeof window.kiwiConfig === 'function') {
+    try {
+        configObj = window.kiwiConfig();
+    } catch (err) {
+        Logger.error('Config file: ' + err.stack);
+        showError();
+    }
 } else if (document.querySelector('meta[name="kiwiconfig"]')) {
     configFile = document.querySelector('meta[name="kiwiconfig"]').content;
 } else if (document.querySelector('script[name="kiwiconfig"]')) {
