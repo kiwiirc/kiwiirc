600e1b59eb20f0d8be61c44da771c6e95ba2c938
Darren <darren@darrenwhitlen.com>
2017-07-20 12:17:32 +0100

    Minor main.js refactor

--- src/main.js
+++ src/main.js
@@ -56,47 +56,54 @@ Vue.mixin({
     },
 });
 
-let configFile = 'static/config.json';
-let configObj = null;
 
-/**
- * Finding the config file
- * In order, looks in the following places:
- *   1. If a 'config' argument is in the query string, static/config_THEVALUE_.json
- *   2. If a window.kiwiConfig function exists, use it's return value as the config object.
- *   3. If a <meta name="kiwiconfig" content=""> is found, then the content becomes the config URL.
- *   4. If a <script name="kiwiconfig" type="application/json"></script> is found, then the content
- *      becomes the config JSON without making another web request.
- */
-if (getQueryVariable('config')) {
-    configFile = 'static/config_' + getQueryVariable('config') + '.json';
-} else if (typeof window.kiwiConfig === 'function') {
-    try {
-        configObj = window.kiwiConfig();
-    } catch (err) {
-        Logger.error('Config file: ' + err.stack);
-        showError();
-    }
-} else if (document.querySelector('meta[name="kiwiconfig"]')) {
-    configFile = document.querySelector('meta[name="kiwiconfig"]').content;
-} else if (document.querySelector('script[name="kiwiconfig"]')) {
-    let configContents = document.querySelector('script[name="kiwiconfig"]').innerHTML;
+loadApp();
 
-    try {
-        configObj = JSON.parse(configContents);
-    } catch (parseErr) {
-        Logger.error('Config file: ' + parseErr.stack);
-        showError();
+
+function loadApp() {
+    let configFile = 'static/config.json';
+    let configObj = null;
+
+    /**
+     * Finding the config file
+     * In order, looks in the following places:
+     *   1. If a 'config' argument is in the query string, static/config_THEVALUE_.json
+     *   2. If a window.kiwiConfig function exists, use it's return value as the config object.
+     *   3. If a <meta name="kiwiconfig" content=""> is found, then the content becomes the config
+     *      URL.
+     *   4. If a <script name="kiwiconfig" type="application/json"></script> is found, then the
+     *      content becomes the config JSON without making another web request.
+     */
+    if (getQueryVariable('config')) {
+        configFile = 'static/config_' + getQueryVariable('config') + '.json';
+    } else if (typeof window.kiwiConfig === 'function') {
+        try {
+            configObj = window.kiwiConfig();
+        } catch (err) {
+            Logger.error('Config file: ' + err.stack);
+            showError();
+        }
+    } else if (document.querySelector('meta[name="kiwiconfig"]')) {
+        configFile = document.querySelector('meta[name="kiwiconfig"]').content;
+    } else if (document.querySelector('script[name="kiwiconfig"]')) {
+        let configContents = document.querySelector('script[name="kiwiconfig"]').innerHTML;
+
+        try {
+            configObj = JSON.parse(configContents);
+        } catch (parseErr) {
+            Logger.error('Config file: ' + parseErr.stack);
+            showError();
+        }
     }
+
+    let configLoader = new ConfigLoader();
+    (configObj ? configLoader.loadFromObj(configObj) : configLoader.loadFromUrl(configFile))
+        .then(applyConfig)
+        .then(initState)
+        .then(initLocales)
+        .then(startApp)
+        .catch(showError);
 }
-
-let configLoader = new ConfigLoader();
-(configObj ? configLoader.loadFromObj(configObj) : configLoader.loadFromUrl(configFile))
-    .then(applyConfig)
-    .then(initState)
-    .then(initLocales)
-	.then(startApp)
-	.catch(showError);
 
 
 function applyConfig(config) {
