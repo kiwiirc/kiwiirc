dd761b7b481a08ac01f295184a6e8aaa24e870bf
Darren <darren@darrenwhitlen.com>
2017-08-14 15:33:16 +0100

    ServerView scrolling fixes for IOS

--- src/components/ServerView.vue
+++ src/components/ServerView.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="kiwi-serverview">
-        <tabbed-view :key="network.id">
-            <tabbed-tab :header="'Messages'" :focus="hasMessages">
-                <message-list :buffer="serverBuffer" :messages="serverBuffer.getMessages()"></message-list>
-            </tabbed-tab>
-            <tabbed-tab :header="$t('settings')" :focus="!hasMessages">
-                <network-settings :network="network"></network-settings>
-            </tabbed-tab>
-            <tabbed-tab :header="'Channels'" v-if="network.state==='connected'">
-                <channel-list :network="network"></channel-list>
-            </tabbed-tab>
-        </tabbed-view>
+        <div class="kiwi-serverview-inner">
+            <tabbed-view :key="network.id">
+                <tabbed-tab :header="'Messages'" :focus="hasMessages">
+                    <message-list :buffer="serverBuffer" :messages="serverBuffer.getMessages()"></message-list>
+                </tabbed-tab>
+                <tabbed-tab :header="$t('settings')" :focus="!hasMessages">
+                    <network-settings :network="network"></network-settings>
+                </tabbed-tab>
+                <tabbed-tab :header="'Channels'" v-if="network.state==='connected'">
+                    <channel-list :network="network"></channel-list>
+                </tabbed-tab>
+            </tabbed-view>
+        </div>
     </div>
 </template>
 
@@ -47,6 +49,15 @@ export default {
 .kiwi-serverview {
     box-sizing: border-box;
     overflow: hidden;
+    position: relative;
+}
+.kiwi-serverview-inner {
+    /* Mobile safari can't work out the height for scrolling to work without this wrapper element */
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
 }
 .kiwi-serverview-alerts {
     margin-bottom: 1em;
