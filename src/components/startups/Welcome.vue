e5f6c122c082b37564c1f103a6202c0942c6b2c6
Darren <darren@darrenwhitlen.com>
2017-09-02 20:13:31 +0100

    New welcome screen layout

--- src/components/startups/Welcome.vue
+++ src/components/startups/Welcome.vue
@@ -1,27 +1,33 @@
 <template>
-    <div class="kiwi-welcome-simple">
-        <h2 v-html="greetingText"></h2>
+    <div class="kiwi-welcome-simple" :class="[closing ? 'kiwi-welcome-simple--closing' : '']">
 
-        <template v-if="!network">
-            <form @submit.prevent="formSubmit" class="u-form kiwi-welcome-simple-form">
-                <input-text v-if="showNick" class="kiwi-welcome-simple-nick" :label="$t('nick')" v-model="nick" />
-                <label v-if="showPass" class="kiwi-welcome-simple-have-password">
-                    <input type="checkbox" v-model="show_password_box" /> {{$t('password_have')}}
-                </label>
-                <input-text v-if="show_password_box" class="kiwi-welcome-simple-password" :label="$t('password')" v-model="password" type="password" />
-                <input-text v-if="showChannel" class="kiwi-welcome-simple-channel" :label="$t('channel')" v-model="channel" />
-                <button
-                    class="u-button u-button-primary u-submit kiwi-welcome-simple-start"
-                    type="submit"
-                    v-html="buttonText"
-                    :disabled="!readyToStart"
-                ></button>
-            </form>
-        </template>
-        <template v-else-if="network.state !== 'connected'">
-            <i class="fa fa-spin fa-spinner" style="font-size:2em; margin-top:1em;" aria-hidden="true"></i>
-        </template>
+        <div class="kiwi-welcome-simple-section kiwi-welcome-simple-section-connection">
+            <h2 v-html="greetingText"></h2>
+
+            <template v-if="!network">
+                <form @submit.prevent="formSubmit" class="u-form kiwi-welcome-simple-form">
+                    <input-text v-if="showNick" class="kiwi-welcome-simple-nick" :label="$t('nick')" v-model="nick" />
+                    <label v-if="showPass" class="kiwi-welcome-simple-have-password">
+                        <input type="checkbox" v-model="show_password_box" /> {{$t('password_have')}}
+                    </label>
+                    <input-text v-if="show_password_box" class="kiwi-welcome-simple-password" :label="$t('password')" v-model="password" type="password" />
+                    <input-text v-if="showChannel" class="kiwi-welcome-simple-channel" :label="$t('channel')" v-model="channel" />
+                    <button
+                        class="u-button u-button-primary u-submit kiwi-welcome-simple-start"
+                        type="submit"
+                        v-html="buttonText"
+                        :disabled="!readyToStart"
+                    ></button>
+                </form>
+            </template>
+            <template v-else-if="network.state !== 'connected'">
+                <i class="fa fa-spin fa-spinner" style="font-size:2em; margin-top:1em;" aria-hidden="true"></i>
+            </template>
+        </div>
         
+        <div class="kiwi-welcome-simple-section kiwi-welcome-simple-section-info" :style="infoStyle">
+            <div class="kiwi-welcome-simple-section-info-content" v-if="infoContent" v-html="infoContent"></div>
+        </div>
     </div>
 </template>
 
@@ -42,6 +48,7 @@ export default {
             showPass: true,
             showNick: true,
             show_password_box: false,
+            closing: false,
         };
     },
     computed: {
@@ -68,8 +75,29 @@ export default {
 
             return ready;
         },
+        infoStyle: function infoStyle() {
+            let style = {};
+            let options = state.settings.startupOptions;
+
+            if (options.infoBackground) {
+                style['background-image'] = `url(${options.infoBackground})`;
+            } else {
+                style['background-color'] = '#333333';
+            }
+
+            return style;
+        },
+        infoContent: function infoContent() {
+            return state.settings.startupOptions.infoContent || '';
+        },
     },
     methods: {
+        close: function close() {
+            this.closing = true;
+            this.$el.addEventListener('transitionend', (event) => {
+                this.$emit('start');
+            }, false);
+        },
         formSubmit: function formSubmit() {
             if (this.readyToStart) {
                 this.startUp();
@@ -106,7 +134,7 @@ export default {
 
             net.ircClient.connect();
             let onRegistered = () => {
-                this.$emit('start');
+                this.close();
                 net.ircClient.off('registered', onRegistered);
                 net.ircClient.off('close', onClosed);
             };
@@ -148,12 +176,125 @@ export default {
 
 .kiwi-welcome-simple {
     height: 100%;
-    overflow-y: auto;
-    box-sizing: border-box;
 }
 
 .kiwi-welcome-simple h2 {
     margin-bottom: 1.5em;
 }
 
+.kiwi-welcome-simple-section {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 50%;
+    padding: 1em;
+    box-sizing: border-box;
+    transition: right 0.3s, left 0.3s;
+    overflow-y: auto;
+}
+
+
+/** Right side */
+.kiwi-welcome-simple-section-info {
+    right: 0;
+    border: 0 solid #86b32d;
+    border-left-width: 5px;
+    background-size: cover;
+    color: #fff;
+    background-position: bottom;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100%;
+}
+.kiwi-welcome-simple-section-info-content {
+    background: rgba(255, 255, 255, 0.74);
+    margin: 2em;
+    color: #1b1b1b;
+    font-size: 1.5em;
+    padding: 2em;
+    line-height: 1.6em;
+}
+
+
+/** Left side */
+.kiwi-welcome-simple-section-connection {
+    left: 0;
+    padding-top: 3em;
+    font-size: 1.2em;
+}
+
+.kiwi-welcome-simple-section-connection label {
+    text-align: left;
+    display: inline-block;
+    margin-bottom: 1.5em;
+}
+.kiwi-welcome-simple-section-connection input[type="text"] {
+    font-size: 1em;
+    margin-top: 5px;
+    padding: 0.3em 1em;
+    width: 100%;
+    box-sizing: border-box;
+}
+
+.kiwi-welcome-simple-start {
+    font-size: 1.1em;
+    cursor: pointer;
+}
+.kiwi-welcome-simple-form {
+    width: 300px;
+    margin: 2em auto;
+}
+
+/** Closing - the wiping away of the screen **/
+.kiwi-welcome-simple--closing .kiwi-welcome-simple-section-connection {
+    left: -50%;
+}
+.kiwi-welcome-simple--closing .kiwi-welcome-simple-section-info {
+    right: -50%;
+}
+
+/** Smaller screen...**/
+@media screen and (max-width: 850px) {
+    .kiwi-welcome-simple {
+        font-size: 0.9em;
+    }
+
+    .kiwi-startbnc-section-connection {
+        margin-top: 1em;
+    }
+    .kiwi-welcome-simple-section-info-content {
+        margin: 1em;
+    }
+}
+
+/** Even smaller screen.. probably phones **/
+@media screen and (max-width: 750px) {
+    .kiwi-welcome-simple {
+        font-size: 0.9em;
+        overflow-y: auto;
+    }
+
+    .kiwi-welcome-simple-section {
+        left: 0;
+        width: 100%;
+        right: auto;
+        position: relative;
+    }
+
+    .kiwi-welcome-simple-section-info {
+        border-width: 5px 0 0 0;
+    }
+    .kiwi-welcome-simple-section-info-content {
+        margin: 0.5em;
+    }
+
+    /** Closing - the wiping away of the screen **/
+    .kiwi-welcome-simple--closing .kiwi-welcome-simple-section-connection {
+        left: -100%;
+    }
+    .kiwi-welcome-simple--closing .kiwi-welcome-simple-section-info {
+        left: -100%;
+    }
+}
 </style>
