c949075ccbbea22b9b8c70c1ea228595e35d9565
ItsOnlyBinary <github@itsonlybinary.com>
2017-09-04 14:24:44 +0100

    Create startup ZncLogin.vue

--- src/components/App.vue
+++ src/components/App.vue
@@ -46,6 +46,7 @@
 import 'font-awesome-webpack';
 
 import startupWelcome from 'src/components/startups/Welcome';
+import startupZncLogin from 'src/components/startups/ZncLogin';
 import startupCustomServer from 'src/components/startups/CustomServer';
 import startupKiwiBnc from 'src/components/startups/KiwiBnc';
 import startupPersonal from 'src/components/startups/Personal';
@@ -121,6 +122,7 @@ export default {
             welcome: startupWelcome,
             customServer: startupCustomServer,
             kiwiBnc: startupKiwiBnc,
+            zncLogin: startupZncLogin,
             personal: startupPersonal,
         };
         let extraStartupScreens = state.getStartups();
