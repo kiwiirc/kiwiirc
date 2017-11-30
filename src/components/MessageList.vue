41efb48deeed2420f2fef79f8b8e032923760048
iwalkalone <iwalkalone69@gmail.com>
2017-12-01 00:09:12 +0100

    refs #141: Adding options to show/hide topics, nick changes and mode changes in buffer settings.

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -88,8 +88,6 @@ export default {
                 '';
         },
         filteredMessages() {
-            let network = this.buffer.getNetwork();
-            let currentNick = network.nick;
             let bufferMessages = this.buffer.getMessages();
 
             // Hack; We need to make vue aware that we depend on buffer.message_count in order to
@@ -113,8 +111,20 @@ export default {
             let list = [];
             let maxSize = this.buffer.setting('scrollback_size');
             let showJoinParts = this.buffer.setting('show_joinparts');
+            let showTopics = this.buffer.setting('show_topics');
+            let showNickChanges = this.buffer.setting('show_nick_changes');
+            let showModeChanges = this.buffer.setting('show_mode_changes');
             for (let i = messages.length - 1; i >= 0 && list.length < maxSize; i--) {
                 if (!showJoinParts && messages[i].type === 'traffic') {
+                    continue;
+                }
+                if (!showTopics && messages[i].type === 'topic') {
+                    continue;
+                }
+                if (!showNickChanges && messages[i].type === 'nick') {
+                    continue;
+                }
+                if (!showModeChanges && messages[i].type === 'mode') {
                     continue;
                 }
                 // Ignored users have the ignore flag set
@@ -122,21 +132,7 @@ export default {
                     continue;
                 }
 
-                // When we join a channel the topic is usually sent next. But this looks
-                // ugly when rendered. So we switch the topic + join messages around so
-                // that the topic is first in the message list.
-                if (
-                    messages[i].type === 'topic' &&
-                    messages[i - 1] &&
-                    messages[i - 1].type === 'traffic' &&
-                    messages[i - 1].nick === currentNick
-                ) {
-                    list.push(messages[i - 1]);
-                    list.push(messages[i]);
-                    i--;
-                } else {
-                    list.push(messages[i]);
-                }
+                list.push(messages[i]);
             }
 
             return list.reverse();
