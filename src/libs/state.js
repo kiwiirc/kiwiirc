b274190490e58588a28ce92acf8195a838009cb7
ItsOnlyBinary <github@itsonlybinary.com>
2018-01-18 19:31:55 +0000

    keep track of last viewed buffer for each network

--- src/libs/state.js
+++ src/libs/state.js
@@ -640,6 +640,11 @@ const state = new Vue({
                 this.ui.active_network = 0;
                 this.ui.active_buffer = '';
             } else {
+                let network = this.getNetwork(networkid);
+                if (network) {
+                    network.lastActiveBuffer = this.ui.active_buffer;
+                }
+
                 this.ui.active_network = networkid;
                 this.ui.active_buffer = bufferName;
 
@@ -652,6 +657,28 @@ const state = new Vue({
                 // Update the buffers last read time
                 buffer.markAsRead(true);
             }
+        },
+
+        setLastActiveBuffer: function setLastActiveBuffer(networkid) {
+            let target;
+            let network = this.getNetwork(networkid);
+            if (network && network.lastActiveBuffer) {
+                for (let b in network.buffers) {
+                    if (network.buffers[b] &&
+                        network.buffers[b].name === network.lastActiveBuffer
+                    ) {
+                        target = network.lastActiveBuffer;
+                    }
+                }
+            }
+
+            if (!target) {
+                target = network ?
+                    network.serverBuffer().name :
+                    '';
+            }
+
+            this.setActiveBuffer(networkid, target);
         },
 
         updateBufferLastRead: function updateBufferLastRead(networkid, bufferName) {
@@ -745,10 +772,7 @@ const state = new Vue({
             }
 
             if (isActiveBuffer) {
-                let target = network.buffers.length > 1 ?
-                    network.buffers[1].name :
-                    network.serverBuffer().name;
-                this.setActiveBuffer(network.id, target);
+                this.setLastActiveBuffer(network.id);
             }
 
             // Remove this buffer from any users
