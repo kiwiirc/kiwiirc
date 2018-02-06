69ab3d2552d481ed35c9c87765917a0c3b5938d5
Darren <darren@darrenwhitlen.com>
2018-02-06 11:52:30 +0000

    Forward state event bus events to the global plugin API

--- src/libs/GlobalApi.js
+++ src/libs/GlobalApi.js
@@ -1,13 +1,14 @@
-import eventEmitter from 'event-emitter';
+import EventEmitter from 'eventemitter3';
 import Vue from 'vue';
 import Logger from './Logger';
 
 let singletonInstance = null;
-let plugins = [];
+let pluginsToInit = [];
 
-export default class GlobalApi {
+export default class GlobalApi extends EventEmitter {
     constructor() {
-        eventEmitter(this);
+        super();
+
         this.Vue = Vue;
         this.state = null;
         this.themes = null;
@@ -34,14 +35,14 @@ export default class GlobalApi {
         if (this.isReady) {
             this.initPlugin(plugin);
         } else {
-            plugins.push(plugin);
+            pluginsToInit.push(plugin);
         }
     }
 
     // Init any plugins that were added before we were ready
     initPlugins() {
-        plugins.forEach(plugin => this.initPlugin(plugin));
-        plugins = [];
+        pluginsToInit.forEach(plugin => this.initPlugin(plugin));
+        pluginsToInit = [];
     }
 
     initPlugin(plugin) {
@@ -55,6 +56,20 @@ export default class GlobalApi {
 
     setState(state) {
         this.state = state;
+
+        // Hacky, but since Vues emitter doesnt support 'all', hijack its $emit call
+        // so that we can forward the event on to plugins
+        let oldEmit = this.state.$emit;
+        this.state.$emit = (...args) => {
+            try {
+                this.emit('all', ...args.slice(1));
+                this.emit(...args);
+            } catch (err) {
+                Logger.error(err.stack);
+            }
+
+            return oldEmit.call(this.state, ...args);
+        };
     }
 
     setThemeManager(themeManager) {
