0dffa3a7e93e8ffb491a8f325a6b137206e617d2
Allan McKernan <meagusxlr@hotmail.com>
2018-08-03 12:50:04 +0100

    - Updating the Channel list to fit in with the new modern look of kiwi.

--- src/components/ChannelList.vue
+++ src/components/ChannelList.vue
@@ -1,46 +1,46 @@
 <template>
-    <div class="kiwi-channellist">
-        <div class="kiwi-channellist-nav">
-            <a
-                :class="{
-                    'u-button-primary': !isLoading,
-                    'u-button-secondary': isLoading,
-                }"
-                class="u-button kiwi-channellist-refresh"
-                @click="maybeUpdateList">
-                <i v-if="!isLoading" class="fa fa-refresh" aria-hidden="true"/>
-                <i v-else class="fa fa-refresh fa-spin" aria-hidden="true"/>
-            </a>
-
-            <div class="kiwi-channellist-pagination">
-                <a @click="prevPage"><i class="fa fa-step-backward" aria-hidden="true"/></a>
-                {{ page + 1 }} / {{ maxPages + 1 }}
-                <a @click="nextPage"><i class="fa fa-step-forward" aria-hidden="true"/></a>
+    <div :class="{'kiwi-channellist-padding-top': list.length == 0}" class="kiwi-channellist">
+        <div class="kiwi-channellist-content-container">
+            <div class="kiwi-channellist-nav">
+                <form class="u-form kiwi-channellist-search" @submit.prevent>
+                    <input v-model="search" :placeholder="$t('do_search')" class="u-input" >
+                    <a
+                        :class="{
+                            'u-button-primary': !isLoading,
+                            'u-button-secondary': isLoading,
+                        }"
+                        class="u-button kiwi-channellist-refresh"
+                        @click="maybeUpdateList">
+                        <i v-if="!isLoading" class="fa fa-refresh" aria-hidden="true"/>
+                        <i v-else class="fa fa-refresh fa-spin" aria-hidden="true"/>
+                    </a>
+                </form>
+                <div v-if="paginated && !isLoading" class="kiwi-channellist-pagination">
+                    <a @click="prevPage"><i class="fa fa-step-backward" aria-hidden="true"/></a>
+                    {{ page + 1 }} / {{ maxPages + 1 }}
+                    <a @click="nextPage"><i class="fa fa-step-forward" aria-hidden="true"/></a>
+                </div>
             </div>
-
-            <form class="u-form kiwi-channellist-search" @submit.prevent>
-                <input v-model="search" :placeholder="$t('do_search')" class="u-input" >
-            </form>
+            <table v-if="!isLoading && list.length > 0" :key="last_updated" width="100%">
+                <tbody>
+                    <tr v-for="channel in paginated" :key="channel.channel">
+                        <td>
+                            <span v-if="channel.num_users >= 0" class="kiwi-channellist-users">
+                                <i class="fa fa-user" aria-hidden="true"/> {{ channel.num_users }}
+                            </span>
+                            <a class="u-link" @click="joinChannel(channel.channel)">
+                                {{ channel.channel }}
+                            </a>
+                        </td>
+                        <td>{{ channel.topic }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div v-else-if="noResults" class="kiwi-channellist-info">
+                {{ $t('channel_list_nonefound') }}
+            </div>
+            <div v-else class="kiwi-channellist-info">{{ $t('channel_list_fetch') }}</div>
         </div>
-        <table v-if="!isLoading && list.length > 0" :key="last_updated" width="100%">
-            <tbody>
-                <tr v-for="channel in paginated" :key="channel.channel">
-                    <td>
-                        <span v-if="channel.num_users >= 0" class="kiwi-channellist-users">
-                            <i class="fa fa-user" aria-hidden="true"/> {{ channel.num_users }}
-                        </span>
-                        <a class="u-link" @click="joinChannel(channel.channel)">
-                            {{ channel.channel }}
-                        </a>
-                    </td>
-                    <td>{{ channel.topic }}</td>
-                </tr>
-            </tbody>
-        </table>
-        <div v-else-if="noResults" class="kiwi-channellist-info">
-            {{ $t('channel_list_nonefound') }}
-        </div>
-        <div v-else class="kiwi-channellist-info">{{ $t('channel_list_fetch') }}</div>
     </div>
 </template>
 
@@ -148,12 +148,50 @@ export default {
 
 .kiwi-channellist {
     box-sizing: border-box;
+    text-align: center;
+    transition: all 0.6s;
+}
+
+.kiwi-channellist-padding-top {
+    padding-top: 250px;
+}
+
+.kiwi-channellist-padding-top .kiwi-channellist-nav {
+    width: 100%;
+    text-align: center;
 }
 
 .kiwi-channellist-nav {
+    padding: 10px 20px;
+    box-sizing: border-box;
+}
+
+/* Input form styling */
+.kiwi-channellist-nav .u-form {
+    padding-right: 46px;
+    position: relative;
+}
+
+.kiwi-channellist-nav .u-form .u-input {
+    width: 324px;
+}
+
+.kiwi-channellist-nav .u-form .u-button-primary,
+.kiwi-channellist-nav .u-form .u-button-secondary {
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 24px;
+    width: 12px;
     text-align: center;
-    margin-top: 10px;
-    margin-bottom: 10px;
+    line-height: 26px;
+    font-size: 1.3em;
+    border-radius: 0;
+}
+
+.kiwi-channellist-nav .u-form .u-button-primary i,
+.kiwi-channellist-nav .u-form .u-button-secondary i {
+    margin-left: -2px;
 }
 
 .kiwi-channellist-pagination {
@@ -174,7 +212,6 @@ export default {
 
 .kiwi-channellist-info {
     text-align: center;
-    padding: 2em 0;
 }
 
 .kiwi-channellist table {
