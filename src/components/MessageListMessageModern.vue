bc34e0af17632e6819c92cbb500dd4f08a972fe5
ItsOnlyBinary <github@itsonlybinary.com>
2018-12-18 14:37:14 +0000

    Add message bodyTemplate

--- src/components/MessageListMessageModern.vue
+++ src/components/MessageListMessageModern.vue
@@ -55,7 +55,12 @@
             >
                 {{ ml.formatTime(message.time) }}
             </div>
-            <div class="kiwi-messagelist-body" v-html="ml.formatMessage(message)"/>
+            <div
+                v-rawElement="message.bodyTemplate.$el"
+                v-if="message.bodyTemplate && message.bodyTemplate.$el"
+                class="kiwi-messagelist-body"
+            />
+            <div v-else class="kiwi-messagelist-body" v-html="ml.formatMessage(message)"/>
 
             <message-info
                 v-if="ml.message_info_open===message"
