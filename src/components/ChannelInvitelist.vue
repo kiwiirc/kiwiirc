88cfe91943885726374ced05cc11870a03a2b77d
Darren <darren@darrenwhitlen.com>
2019-01-20 16:27:15 +0000

    Linting

--- src/components/ChannelInvitelist.vue
+++ src/components/ChannelInvitelist.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class="kiwi-channelinvitelist">
-        <form class="u-form kiwi-channelinvitelist" @submit.prevent="">
+    <div class="kiwi-invitelist">
+        <form class="u-form kiwi-invitelist" @submit.prevent="">
             <a class="u-link" @click="updateInvitelist">{{ $t('invites_refresh') }}</a>
 
             <div v-if="!channelIsInviteOnly">
                 This is a public channel.
                 <a
                     v-if="areWeAnOp"
-                    @click="setInviteOnly"
                     class="u-link"
+                    @click="setInviteOnly"
                 >Only allow invited users</a>
             </div>
 
             <span v-if="anyRegisteredUserCanJoin">Any registered user can join</span>
 
             <div v-if="supportsAccounts">
-                <h3>Invited users ({{inviteListAccounts.length}})</h3>
+                <h3>Invited users ({{ inviteListAccounts.length }})</h3>
                 <div v-if="knownAccounts.length > 0 && areWeAnOp">
                     <select ref="addInviteList">
                         <option
                             v-for="user in knownAccounts"
                             :key="user.nick" :value="user.account"
-                        >{{user.account}}</option>
+                        >{{ user.account }}</option>
                     </select>
                     <button @click="addAccountInvite($refs.addInviteList.value)">Add invite</button>
                 </div>
 
-                <table v-if="inviteListAccounts.length > 0" class="kiwi-channelinvitelist-table">
+                <table v-if="inviteListAccounts.length > 0" class="kiwi-invitelist-table">
                     <tr>
                         <th>{{ $t('invites_user') }}</th>
                         <th>{{ $t('invites_by') }}</th>
@@ -34,14 +34,21 @@
                         <th/>
                     </tr>
                     <tr v-for="invite in inviteListAccounts" :key="invite.invited">
-                        <td class="kiwi-channelinvitelist-table-mask">{{ displayMask(invite) }}</td>
-                        <td class="kiwi-channelinvitelist-table-invitedby">{{ invite.invited_by }}</td>
-                        <td class="kiwi-channelinvitelist-table-invitedat">
+                        <td class="kiwi-invitelist-table-mask">
+                            {{ displayMask(invite) }}
+                        </td>
+                        <td class="kiwi-invitelist-table-invitedby">
+                            {{ invite.invited_by }}
+                        </td>
+                        <td class="kiwi-invitelist-table-invitedat">
                             {{ (new Date(invite.invited_at * 1000)).toDateString() }}
                         </td>
-                        <td class="kiwi-channelinvitelist-table-actions">
-                            <i class="fa fa-trash" aria-hidden="true"
-                            @click="removeInvite(invite.invited)"/>
+                        <td class="kiwi-invitelist-table-actions">
+                            <i
+                                class="fa fa-trash"
+                                aria-hidden="true"
+                                @click="removeInvite(invite.invited)"
+                            />
                         </td>
                     </tr>
                 </table>
@@ -49,7 +56,7 @@
 
             <div v-if="supportsAccounts && inviteListNonAccounts.length > 0">
                 <h3>Invite matches</h3>
-                <table class="kiwi-channelinvitelist-table">
+                <table class="kiwi-invitelist-table">
                     <tr>
                         <th>{{ $t('invites_user') }}</th>
                         <th>{{ $t('invites_by') }}</th>
@@ -57,14 +64,21 @@
                         <th/>
                     </tr>
                     <tr v-for="invite in inviteListNonAccounts" :key="invite.invited">
-                        <td class="kiwi-channelinvitelist-table-mask">{{ invite.invited }}</td>
-                        <td class="kiwi-channelinvitelist-table-invitedby">{{ invite.invited_by }}</td>
-                        <td class="kiwi-channelinvitelist-table-invitedat">
+                        <td class="kiwi-invitelist-table-mask">
+                            {{ invite.invited }}
+                        </td>
+                        <td class="kiwi-invitelist-table-invitedby">
+                            {{ invite.invited_by }}
+                        </td>
+                        <td class="kiwi-invitelist-table-invitedat">
                             {{ (new Date(invite.invited_at * 1000)).toDateString() }}
                         </td>
-                        <td class="kiwi-channelinvitelist-table-actions">
-                            <i class="fa fa-trash" aria-hidden="true"
-                            @click="removeInvite(invite.invited)"/>
+                        <td class="kiwi-invitelist-table-actions">
+                            <i
+                                class="fa fa-trash"
+                                aria-hidden="true"
+                                @click="removeInvite(invite.invited)"
+                            />
                         </td>
                     </tr>
                 </table>
@@ -82,6 +96,12 @@ import * as IrcdDiffs from '@/helpers/IrcdDiffs';
 
 export default {
     props: ['buffer'],
+    data() {
+        return {
+            inviteList: [],
+            is_refreshing: false,
+        };
+    },
     computed: {
         supportsAccounts() {
             return !!this.extban;
@@ -90,7 +110,7 @@ export default {
             return IrcdDiffs.extbanAccount(this.buffer.getNetwork());
         },
         inviteListAccounts() {
-            let accounts = this.inviteList.filter((i) => i.invited.indexOf(this.extban) === 0);
+            let accounts = this.inviteList.filter(i => i.invited.indexOf(this.extban) === 0);
             accounts.sort((a, b) => {
                 let aMask = a.invited.toUpperCase();
                 let bMask = b.invited.toUpperCase();
@@ -113,7 +133,7 @@ export default {
                 return this.inviteList;
             }
 
-            return this.inviteList.filter((i) => i.invited.indexOf(this.extban) !== 0);
+            return this.inviteList.filter(i => i.invited.indexOf(this.extban) !== 0);
         },
         channelIsInviteOnly() {
             return typeof this.buffer.modes.i !== 'undefined';
@@ -125,7 +145,7 @@ export default {
 
             let extban = this.extban;
             // Find any invite that only consists of the extban and nothing else. Eg. '~a:'
-            return !!this.inviteListAccounts.find((invite) => invite.invited === extban + ':');
+            return !!this.inviteListAccounts.find(invite => invite.invited === extban + ':');
         },
         areWeAnOp() {
             return this.buffer.isUserAnOp(this.buffer.getNetwork().nick);
@@ -137,31 +157,25 @@ export default {
             let extban = this.extban;
             let inviteAccountNames = this.inviteListAccounts.map((i) => {
                 let mask = i.invited;
-                return mask.replace(extban + ':', '')
+                return mask.replace(extban + ':', '');
             });
 
             let accountUsers = [];
-            for (let i in users) {
-                if (users[i].account && inviteAccountNames.indexOf(users[i].account) === -1) {
-                    accountUsers.push(users[i]);
+            Object.values(users).forEach((user) => {
+                if (users.account && inviteAccountNames.indexOf(users.account) === -1) {
+                    accountUsers.push(users);
                 }
-            }
+            });
 
             return accountUsers;
         },
-    },
-    data() {
-        return {
-            inviteList: [],
-            is_refreshing: false,
-        };
     },
     created() {
         this.updateInvitelist();
     },
     methods: {
         displayMask(invite) {
-            let display = invite.invited.replace(this.extban+':', '');
+            let display = invite.invited.replace(this.extban + ':', '');
             display = display || '(Any registered user)';
             return display;
         },
@@ -193,18 +207,18 @@ export default {
 };
 </script>
 <style lang="less">
-.kiwi-channelinvitelist-table {
+.kiwi-invitelist-table {
     width: 100%;
     border-collapse: collapse;
     line-height: 20px;
     margin-top: 10px;
 }
 
-.kiwi-channelinvitelist-table-invitedat {
+.kiwi-invitelist-table-invitedat {
     min-width: 150px;
 }
 
-.kiwi-channelinvitelist-table-actions {
+.kiwi-invitelist-table-actions {
     min-width: 50px;
     text-align: center;
     cursor: pointer;
