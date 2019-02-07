ce73c9e37cd6edeabf94cdaae3f15be66ce04e74
Allan Mckernan <workofcode@gmail.com>
2019-02-07 12:02:24 +0000

    - ADD: added missing files ( AlertForgetMe, Salert)

---
+++ src/components/Salert.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="kiwi-salert-content">
+        <h1>New Conversation</h1>
+        <multiselect v-model="value" :multiple="true" :options="options" />
+        <p><small>*Selecting multiple users will create a new private channel.</small></p>
+    </div>
+</template>
+
+<script>
+'kiwi public';
+
+import Multiselect from 'vue-multiselect';
+
+export default {
+    components: {
+        Multiselect,
+    },
+    data: function data() {
+        return {
+            value: [],
+            options: ['username1', 'username2', 'username3', 'username4', 'username5', 'username6'],
+        };
+    },
+};
+</script>
+
+<style lang="less">
+    .kiwi-salert-content {
+        font-family: Source Sans Pro, Helvetica, sans-serif;
+        margin-bottom: -20px;
+    }
+
+    .kiwi-salert-content h1 {
+        margin: 0 0 20px 0;
+        padding: 0;
+    }
+</style>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
---
+++ src/components/alerts/alertForgetMe.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="kiwi-salert-content">
+        <h1>Delete local data?</h1>
+        <p>Are you sure you want to delete all locally stored data?.
+        This action cannot be undone.</p>
+    </div>
+</template>
+
+<script>
+'kiwi public';
+
+export default {
+    data: function data() {
+        return {
+        };
+    },
+};
+</script>
+
+<style lang="less">
+    .kiwi-salert-content {
+        font-family: Source Sans Pro, Helvetica, sans-serif;
+        margin-bottom: -20px;
+    }
+
+    .kiwi-salert-content h1 {
+        margin: 0 0 20px 0;
+        padding: 0;
+    }
+
+    .swal-overlay {
+        background-color: rgba(84, 84, 84, 0.6);
+    }
+
+    .swal-button-danger {
+        background-color: #cc5656;
+        transition: background-color 0.3s;
+    }
+</style>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
