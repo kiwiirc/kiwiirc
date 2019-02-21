9412581a70dcdbddbc4473a373a2607a663315e8
Allan <workofcode@gmail.com>
2019-02-21 11:47:24 +0000

    Align channel topic on ModernLayout with other messages. (#821)
    * UPDATE: The topic on modernLayout is now inline with the rest of the Messages, looks much cleaner.
    
    * - Add margin to the top of the topic message

--- src/components/MessageListMessageModern.vue
+++ src/components/MessageListMessageModern.vue
@@ -233,7 +233,7 @@ export default {
 }
 
 .kiwi-messagelist-message--modern.kiwi-messagelist-message-topic {
-    margin: 10px 20px 10px 20px;
+    margin: 20px 20px 20px 66px;
 }
 
 .kiwi-messagelist-message--modern.kiwi-messagelist-message-topic .kiwi-messagelist-modern-left {
@@ -396,6 +396,10 @@ export default {
 
     .kiwi-messagelist-message--modern.kiwi-messagelist-message-traffic {
         padding-left: 10px;
+    }
+
+    .kiwi-messagelist-message--modern.kiwi-messagelist-message-topic {
+        margin: 0 15px 20px 15px;
     }
 }
 </style>
