1038cf3ea1fc1180e4bbf017615fea987f9c2f3a
Allan Mckernan <workofcode@gmail.com>
2019-02-27 16:03:50 +0000

    - Made width topic changes  more specific

--- src/components/MessageListMessageModern.vue
+++ src/components/MessageListMessageModern.vue
@@ -219,8 +219,8 @@ export default {
     padding-top: 0;
 }
 
-.kiwi-messagelist-message-topic,
-.kiwi-messagelist-message--authorrepeat.kiwi-messagelist-message-topic {
+.kiwi-messagelist-message--modern.kiwi-messagelist-message-topic,
+.kiwi-messagelist-message--modern.kiwi-messagelist-message--authorrepeat.kiwi-messagelist-message-topic {
     padding-top: 10px;
     padding-bottom: 10px;
     margin: 10px 2.5%;
