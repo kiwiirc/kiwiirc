bf3cc4312479d2d5238dbe6584b635bb06df37e7
Allan Mckernan <workofcode@gmail.com>
2019-02-28 11:10:40 +0000

    - Statebrowser line height change - Dark theme upgrade to variable based file

--- src/components/StateBrowser.vue
+++ src/components/StateBrowser.vue
@@ -343,7 +343,7 @@ export default {
     padding: 0 10px;
     margin: 0;
     opacity: 1;
-    line-height: 40px;
+    line-height: 39px;
     cursor: pointer;
     display: block;
     box-sizing: border-box;
