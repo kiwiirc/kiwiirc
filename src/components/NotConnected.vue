a17db8f7b482cc4becb9f4864db2f78e42980e6a
Allan Mckernan <workofcode@gmail.com>
2019-04-11 11:55:45 +0100

    - Remove styling of disconnected/connected messages in message list - Mobile styling of the notConnected component - Remove ICON from the translations for 'connected' / 'disconnected' - Remove embedded colours in the modernMessagelist layout - should not be there

--- src/components/NotConnected.vue
+++ src/components/NotConnected.vue
@@ -1,13 +1,10 @@
 <template>
     <div :class="{ connecting: shouldShowLoading }" class="kiwi-notconnected kiwi-warning-block">
-        <div class="kiwi-notconnected-bigicon">
-            <i v-if="!shouldShowLoading" class="fa fa-frown-o" aria-hidden="true"/>
-            <i v-else class="fa fa-refresh fa-spin kiwi-notconnected-bigicon" aria-hidden="true"/>
-        </div>
-
         <template v-if="!shouldShowLoading">
             <div class="kiwi-notconnected-caption">
                 <span>{{ $t('not_connected') }}</span>
+                <i v-if="!shouldShowLoading" class="fa fa-frown-o" aria-hidden="true"/>
+                <i v-else class="fa fa-refresh fa-spin kiwi-notconnected-bigicon" aria-hidden="true"/>
             </div>
             <div class="kiwi-notconnected-buttons">
                 <template v-if="isChannel()">
@@ -113,48 +110,50 @@ export default {
 
 <style lang="less">
 .kiwi-notconnected {
+    position: absolute;
+    left: 0;
+    top: 46px;
+    width: 100%;
     box-sizing: border-box;
-    text-align: center;
-    padding: 2% 0;
-    margin: 0;
+    padding: 5px 20px;
     transition: background-color 0.3s;
 }
 
-.kiwi-notconnected-bigicon {
-    display: inline-block;
-    margin: 0 0 0.5em 0;
-}
-
-.kiwi-notconnected-bigicon i {
-    font-size: 4em;
-    cursor: default;
+.kiwi-notconnected.connecting {
+    text-align: center;
 }
 
 .kiwi-notconnected-caption {
-    display: block;
-    width: 100%;
-    font-size: 1.6em;
+    display: inline-block;
+    width: auto;
     cursor: default;
-    margin: 0 0 0.5em 0;
+    line-height: 50px;
+    font-size: 1.2em;
+    font-weight: 900;
+}
+
+.kiwi-notconnected-caption i {
+    font-weight: normal;
 }
 
 .kiwi-notconnected-buttons {
-    width: 100%;
+    float: right;
+    width: auto;
     text-align: center;
-    font-size: 1.6em;
-    padding-top: 0.5em;
 }
 
 .kiwi-notconnected-button {
     width: auto;
     display: inline-block;
-    padding: 0.8em 1em 0.8em 0.6em;
-    font-size: 0.8em;
-    border-radius: 0.4em;
-    margin: 0 0.8em;
+    padding: 5px 10px;
+    border-radius: 7px;
     cursor: pointer;
     border: 2px solid;
-    transition: all 0.3s;
+    margin: 6px 0 0 10px;
+    height: 37px;
+    overflow: hidden;
+    box-sizing: border-box;
+    transition: opacity 0.3s, color 0.3s, background-color 0.3s;
 }
 
 .kiwi-notconnected-button:hover {
@@ -164,20 +163,12 @@ export default {
 .kiwi-notconnected-button i {
     float: left;
     font-size: 1.6em;
-    line-height: 0.8em;
-    margin-right: 0.4em;
+    line-height: 24px;
+    margin-right: 5px;
 }
 
 .kiwi-notconnected-button-settings {
-    font-size: 0.6em;
-    clear: both;
-    display: block;
-    max-width: 160px;
-    margin: 1em auto;
-}
-
-.kiwi-notconnected-button-settings i {
-    line-height: 1em;
+    opacity: 0.8;
 }
 
 @media screen and (max-width: 1024px) {
@@ -210,8 +201,22 @@ export default {
         font-size: 0.8em;
     }
 
+    .kiwi-notconnected-caption {
+        width: 100%;
+        text-align: center;
+        font-size: 1.3em;
+    }
+
+    .kiwi-notconnected-buttons {
+        width: 100%;
+    }
+
     .kiwi-notconnected-button {
-        font-size: 0.7em;
+        font-size: 1em;
+        width: 90%;
+        line-height: 21px;
+        display: inline-block;
+        margin: 0 5%;
     }
 }
 
