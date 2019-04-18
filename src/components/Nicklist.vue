b8cc5dad487155d3a602cceb9e5a024d311e7275
Darren <darren@darrenwhitlen.com>
2019-04-18 16:56:35 +0100

    Close explicitly close the sidebar when opening a query

--- src/components/Nicklist.vue
+++ src/components/Nicklist.vue
@@ -158,7 +158,6 @@ export default {
         openQuery(user) {
             let buffer = this.$state.addBuffer(this.buffer.networkid, user.nick);
             this.$state.setActiveBuffer(buffer.networkid, buffer.name);
-            this.sidebarState.close();
         },
         openUserbox(user) {
             this.$state.$emit('userbox.show', user, {
