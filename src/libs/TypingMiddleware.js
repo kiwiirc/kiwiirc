840d688da5d70c9148c2fe830a1bb6c717c66413
ItsOnlyBinary <git@itsonlybinary.com>
2019-04-28 18:38:42 +0100

    Fix typing notifications in direct messages

--- src/libs/TypingMiddleware.js
+++ src/libs/TypingMiddleware.js
@@ -17,8 +17,11 @@ export default function typingMiddleware() {
             return;
         }
 
+        // if we are params[0] then the target is the sender (direct message)
+        let target = (message.params[0] === client.user.nick) ? message.nick : message.params[0];
+
         client.emit('typing', {
-            target: message.params[0],
+            target: target,
             nick: message.nick,
             ident: message.ident,
             hostname: message.hostname,
