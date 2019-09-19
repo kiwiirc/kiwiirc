eb598678fb71183977d673ebab32be0eaab90937
Darren <darren@darrenwhitlen.com>
2019-09-19 11:54:33 +0100

    Updated inspircd docs link

--- src/helpers/IrcdDiffs.js
+++ src/helpers/IrcdDiffs.js
@@ -57,7 +57,7 @@ export function extbanAccount(network) {
     let prefix = extban.split(',')[0];
     let type = 'a';
 
-    // https://wiki.inspircd.org/2.0/Extbans
+    // https://docs.inspircd.org/3/modules/services_account/#extended-bans
     if (ircdType.indexOf('inspircd') > -1) {
         type = 'R';
     }
