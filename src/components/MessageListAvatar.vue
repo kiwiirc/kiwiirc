92b2d39efe83af652bbabbbfa3b6eec5ddf36527
Darren <darren@darrenwhitlen.com>
2020-01-03 04:57:18 +0000

    MessageListAvatar comments

--- src/components/MessageListAvatar.vue
+++ src/components/MessageListAvatar.vue
@@ -8,6 +8,8 @@
 
 import Avatar from './Avatar';
 
+// This component simply wraps the Avatar component so that plugins may override this messagelist
+// avatar separately than the nicklist avatar
 export default {
     components: { Avatar },
     props: ['message', 'user'],
