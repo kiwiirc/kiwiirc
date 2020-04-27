f08cc8219d3841e40f5e9b84b9399162e710fdcb
Darren <darren@darrenwhitlen.com>
2020-04-27 16:29:21 +0100

    Only show captcha when required

--- src/components/Captcha.vue
+++ src/components/Captcha.vue
@@ -1,6 +1,6 @@
 <template>
-    <div v-if="recaptchaSiteId">
-        <div ref="captchacontainer" />
+    <div>
+        <div v-if="showCaptcha" ref="captchacontainer" />
     </div>
 </template>
 
@@ -9,10 +9,12 @@
 'kiwi public';
 
 export default {
+    props: ['network'],
     data() {
         return {
             recaptchaSiteId: '',
             recaptchaResponse: '',
+            showCaptcha: false,
         };
     },
     computed: {
@@ -26,38 +28,56 @@ export default {
 
         this.listen(this.$state, 'network.connecting', (event) => {
             event.network.ircClient.once('socket connected', () => {
-                // There should always be a captcha response at this point but just in case
-                // there isn't...
                 if (this.recaptchaResponse) {
                     event.network.ircClient.raw('CAPTCHA', this.recaptchaResponse);
                 }
             });
         });
+
+        this.listen(this.$state, 'irc.raw.CAPTCHA', (command, event, network) => {
+            if (network !== this.network) {
+                return;
+            }
+
+            if (event.params[0] === 'NEEDED') {
+                this.loadRecaptcha();
+            }
+        });
     },
     mounted() {
         if (!this.recaptchaSiteId) {
             this.$emit('ready', true);
-            return;
         }
 
-        // Recaptcha calls this callback once it's loaded and ready to be used
-        window.recaptchaLoaded = () => {
-            window.grecaptcha.render(this.$refs.captchacontainer, {
-                sitekey: this.recaptchaSiteId,
-                callback: this.recaptchaSuccess,
-                'expired-callback': this.recaptchaExpired,
-            });
-        };
-
-        let scr = document.createElement('script');
-        scr.src = 'https://www.google.com/recaptcha/api.js?onload=recaptchaLoaded&render=explicit';
-        scr.defer = true;
-        this.$el.appendChild(scr);
+        // this.loadRecaptcha();
     },
     methods: {
+        loadRecaptcha() {
+            this.showCaptcha = true;
+
+            // Recaptcha calls this callback once it's loaded and ready to be used
+            window.recaptchaLoaded = () => {
+                window.grecaptcha.render(this.$refs.captchacontainer, {
+                    sitekey: this.recaptchaSiteId,
+                    callback: this.recaptchaSuccess,
+                    'expired-callback': this.recaptchaExpired,
+                });
+            };
+
+            let scr = document.createElement('script');
+            scr.src = 'https://www.google.com/recaptcha/api.js?onload=recaptchaLoaded&render=explicit';
+            scr.defer = true;
+            this.$el.appendChild(scr);
+        },
         recaptchaSuccess(response) {
             this.recaptchaResponse = response;
             this.$emit('ready', true);
+
+            // If we have a network instance already, send the captcha response
+            if (this.network && this.network.state === 'connecting') {
+                this.network.ircClient.raw('CAPTCHA', response);
+            }
+            this.showCaptcha = false;
         },
         recaptchaExpired() {
             this.recaptchaResponse = '';
