b628d1f1c5334028afffc7a7901c4a453841ad69
Darren <darren@darrenwhitlen.com>
2020-05-15 15:23:09 +0100

    batchedAdd no longer leaking timers

--- src/libs/batchedAdd.js
+++ src/libs/batchedAdd.js
@@ -7,8 +7,9 @@
 export default function batchedAdd(singleFn, batchedFn, numInsertsSec = 3) {
     let queue = [];
     let numInLastSec = 0;
-    let isLooping = false;
+    let queueLoopTmr = null;
     let loopInterval = 1000;
+    let checkSecRateTmr = null;
 
     function queueLoop() {
         numInLastSec = 0;
@@ -18,35 +19,36 @@ export default function batchedAdd(singleFn, batchedFn, numInsertsSec = 3) {
             let q = queue;
             queue = [];
             batchedFn(q);
-            numInLastSec = 0;
-            setTimeout(queueLoop, loopInterval);
+            queueLoopTmr = setTimeout(queueLoop, loopInterval);
         } else {
-            isLooping = false;
+            queueLoopTmr = null;
         }
     }
 
     function maybeStartLoop() {
-        if (isLooping) {
-            return;
+        if (!queueLoopTmr) {
+            queueLoopTmr = setTimeout(queueLoop, loopInterval);
         }
-
-        isLooping = true;
-        setTimeout(queueLoop, loopInterval);
     }
 
-    function resetAddCounter(doResetNum) {
-        if (doResetNum) {
-            numInLastSec = 0;
-        }
-        if (!isLooping) {
-            setTimeout(resetAddCounter, 1000, true);
+    // Reset numInLastSec after loopInterval. This allows enough time for the counter to
+    // increase to detect batching. Only needs to run if we are not currently batching and
+    // only needs to run once at a time.
+    function resetAddCounter() {
+        if (!queueLoopTmr && !checkSecRateTmr) {
+            checkSecRateTmr = setTimeout(() => {
+                checkSecRateTmr = null;
+                if (!queueLoopTmr) {
+                    numInLastSec = 0;
+                }
+            }, loopInterval);
         }
     }
 
     function batchFn(item) {
         numInLastSec++;
 
-        // Under 1 second, queue them
+        // If already queuing or we reached our limit on items/sec, queue the item
         if (queue.length || numInLastSec > numInsertsSec) {
             queue.push(item);
             maybeStartLoop();
