1329cf917694e944b67c55f19f9912609f648836
ItsOnlyBinary <git@itsonlybinary.com>
2020-06-10 14:39:22 +0100

    Send PART before emitting buffer.close event

--- src/libs/state.js
+++ src/libs/state.js
@@ -499,6 +499,10 @@ function createNewState() {
                     return;
                 }
 
+                if (buffer.isChannel() && buffer.joined) {
+                    network.ircClient.part(buffer.name);
+                }
+
                 let eventObj = { buffer };
                 state.$emit('buffer.close', eventObj);
 
@@ -513,10 +517,6 @@ function createNewState() {
                 });
                 if (messageIdx > -1) {
                     messages.splice(messageIdx, 1);
-                }
-
-                if (buffer.isChannel() && buffer.joined) {
-                    network.ircClient.part(buffer.name);
                 }
 
                 // Remove the user from network state if no remaining common channels
