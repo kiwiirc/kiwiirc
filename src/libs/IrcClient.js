2bf1d131e894b6c378784deb244983c2f19f070a
Darren <darren@darrenwhitlen.com>
2020-08-06 10:43:09 +0100

    Remove most IrcClient() constructor options as they are set in .connect() anyway

--- src/libs/IrcClient.js
+++ src/libs/IrcClient.js
@@ -12,36 +12,18 @@ import * as ServerConnection from './ServerConnection';
 export function create(state, network) {
     let networkid = network.id;
 
-    let clientOpts = {
-        host: network.connection.server,
-        port: network.connection.port,
-        tls: network.connection.tls,
-        path: network.connection.path,
-        password: network.connection.password,
-        account: {
-            account: network.connection.nick,
-            password: network.password,
-        },
-        nick: network.connection.nick,
-        username: network.username || network.connection.nick,
-        gecos: network.gecos || 'https://kiwiirc.com/',
+    let ircClient = new Irc.Client({
+        // Most options are set under the overloaded .connect()
         version: null,
-        auto_reconnect: false,
-        encoding: network.connection.encoding,
         message_max_length: 350,
-    };
-
-    let ircClient = new Irc.Client(clientOpts);
+    });
     ircClient.requestCap('znc.in/self-message');
-    // Current version of irc-framework only support draft/message-tags-0.2
-    // TODO: Removee this once irc-framework has been updated
-    ircClient.requestCap('message-tags');
     ircClient.use(chathistoryMiddleware());
     ircClient.use(clientMiddleware(state, network));
     ircClient.use(typingMiddleware());
 
     // Overload the connect() function to make sure we are connecting with the
-    // most recent connection details from the state
+    // most recent connection details from the network state
     let originalIrcClientConnect = ircClient.connect;
     ircClient.connect = function connect(...args) {
         // Set some defaults if we don't have eveything
