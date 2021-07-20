92706b760e2632a4245e67a7c1c86e10a30c7dca
ItsOnlyBinary <git@itsonlybinary.com>
2021-07-20 16:26:15 +0100

    Don't apply style to GlobalAPI userbox button container

--- src/components/UserBox.vue
+++ src/components/UserBox.vue
@@ -45,7 +45,6 @@
                             }
                         }
                     }"
-                    class="kiwi-userbox-action"
                 />
             </div>
         </div>
