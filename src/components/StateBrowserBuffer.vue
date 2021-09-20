9bdd6f61f690076d5598c4ecb7fb42bfda6956b8
ItsOnlyBinary <ItsOnlyBinary@users.noreply.github.com>
2021-09-20 15:21:42 +0100

    Allow closing all queries at once, remove unneeded close buttons from container header (#1579)
    * Allow closing all queries at once, remove unneeded close buttons from container header
    
    * remove dead code from ContainerHeader.vue
    
    * add different close prompt message for queries
    
    * Add channel controls when showBufferGroups is false
    
    * Tweak statebrowser usermenu
    
    * add comment
    
    * Treat special buffers as queries for close prompt

--- src/components/StateBrowserBuffer.vue
+++ src/components/StateBrowserBuffer.vue
@@ -1,45 +1,61 @@
 <template>
-    <div
-        :data-name="buffer.name.toLowerCase()"
-        :class="{
-            'kiwi-statebrowser-channel-active': isActiveBuffer(),
-            'kiwi-statebrowser-channel-notjoined': buffer.isChannel() &&
-                !buffer.joined
-        }"
-        class="kiwi-statebrowser-channel"
-    >
+    <div class="kiwi-statebrowser-channel-wrapper">
         <div
-            class="kiwi-statebrowser-channel-name"
-            @click="$emit('selected')"
+            :data-name="buffer.name.toLowerCase()"
+            :class="{
+                'kiwi-statebrowser-channel-active': isActiveBuffer(),
+                'kiwi-statebrowser-channel-notjoined': buffer.isChannel() &&
+                    !buffer.joined
+            }"
+            class="kiwi-statebrowser-channel"
         >
-            <away-status-indicator
-                v-if="buffer.isQuery() && awayNotifySupported()"
-                :network="network" :user="network.userByName(buffer.name)"
-            />{{ buffer.name }}
-        </div>
-        <div class="kiwi-statebrowser-buffer-actions">
-            <div class="kiwi-statebrowser-channel-labels">
+            <div
+                class="kiwi-statebrowser-channel-name"
+                @click="$emit('selected')"
+            >
+                <away-status-indicator
+                    v-if="buffer.isQuery() && awayNotifySupported()"
+                    :network="network" :user="network.userByName(buffer.name)"
+                />{{ buffer.name }}
+            </div>
+            <div class="kiwi-statebrowser-buffer-actions">
+                <div class="kiwi-statebrowser-channel-labels">
+                    <div
+                        v-if="buffer.flags.unread && showMessageCounts(buffer)"
+                        :class="[
+                            buffer.flags.highlight ?
+                                'kiwi-statebrowser-channel-label--highlight' :
+                                ''
+                        ]"
+                        class="kiwi-statebrowser-channel-label"
+                    >
+                        {{ buffer.flags.unread > 999 ?
+                            "999+": buffer.flags.unread }}
+                    </div>
+                </div>
+
                 <div
-                    v-if="buffer.flags.unread && showMessageCounts(buffer)"
-                    :class="[
-                        buffer.flags.highlight ?
-                            'kiwi-statebrowser-channel-label--highlight' :
-                            ''
-                    ]"
-                    class="kiwi-statebrowser-channel-label"
+                    class="kiwi-statebrowser-channel-leave"
+                    @click="maybePromptClose()"
                 >
-                    {{ buffer.flags.unread > 999 ?
-                        "999+": buffer.flags.unread }}
+                    <i class="fa fa-times" aria-hidden="true" />
                 </div>
             </div>
-
-            <div
-                class="kiwi-statebrowser-channel-leave"
-                @click="closeBuffer(buffer)"
-            >
-                <i class="fa fa-times" aria-hidden="true" />
-            </div>
         </div>
+        <transition-expand>
+            <div v-if="showPromptClose" class="kiwi-statebrowser-prompt-close">
+                <span>{{
+                    buffer.isChannel() ?
+                        $t('prompt_leave_channel') :
+                        $t('prompt_close_query')
+                }}</span>
+                <input-confirm
+                    :flip-connotation="true"
+                    @ok="closeBuffer()"
+                    @submit="maybePromptClose()"
+                />
+            </div>
+        </transition-expand>
     </div>
 </template>
 
@@ -51,10 +67,15 @@ export default {
     components: {
         AwayStatusIndicator,
     },
-    props: ['buffer'],
+    props: ['buffer', 'activePrompt'],
     computed: {
         network() {
             return this.buffer.getNetwork();
+        },
+        showPromptClose() {
+            return (this.activePrompt &&
+                this.activePrompt.type === 'buffer' &&
+                this.activePrompt.value === this.buffer);
         },
     },
     methods: {
@@ -68,11 +89,35 @@ export default {
         awayNotifySupported() {
             return this.network.ircClient.network.cap.isEnabled('away-notify');
         },
-        showMessageCounts(buffer) {
+        showMessageCounts() {
             return !this.buffer.setting('hide_message_counts');
         },
-        closeBuffer(buffer) {
-            this.$state.removeBuffer(buffer);
+        maybePromptClose() {
+            const setting = this.buffer.setting('prompt_leave');
+            if (setting === 'none' || (
+                setting !== 'all' && (
+                    (this.buffer.isChannel() && setting !== 'channels') ||
+                    (this.buffer.isQuery() && setting !== 'queries') ||
+                    (this.buffer.isSpecial() && setting !== 'queries')
+                )
+            )) {
+                // Prompt feature is disabled, just close the buffer
+                this.closeBuffer();
+                return;
+            }
+
+            const prompt = this.activePrompt;
+            if (this.showPromptClose) {
+                // Prompt is currently visible so close it
+                prompt.type = undefined;
+                prompt.value = undefined;
+            } else {
+                prompt.type = 'buffer';
+                prompt.value = this.buffer;
+            }
+        },
+        closeBuffer() {
+            this.$state.removeBuffer(this.buffer);
         },
     },
 };
