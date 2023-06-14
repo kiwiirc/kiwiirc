696c7784d890314dfcb4fdd70f5933e64b393920
ItsOnlyBinary <git@itsonlybinary.com>
2023-06-14 18:06:00 +0100

    Clear input after inviting user

--- src/components/SidebarAboutBuffer.vue
+++ src/components/SidebarAboutBuffer.vue
@@ -165,6 +165,7 @@ export default {
                 return;
             }
             this.network.ircClient.invite(this.buffer.name, this.inviteNick);
+            this.inviteNick = '';
         },
         inviteSelected(value, item) {
             this.inviteNick = value;
