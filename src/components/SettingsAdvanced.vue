2c4c5820945dfeb02612acf9faf93743aaadd53c
ItsOnlyBinary <ItsOnlyBinary@users.noreply.github.com>
2025-03-06 21:16:41 +0000

    Improve SettingsAdvanced.vue (#1914)

--- src/components/SettingsAdvanced.vue
+++ src/components/SettingsAdvanced.vue
@@ -2,7 +2,7 @@
     <div class="kiwi-settings-advanced">
         <div class="kiwi-settings-advanced-notice">{{ $t('settings_advanced_header') }}</div>
         <form class="u-form">
-            <div class="kiwi-settings-advanced-filter-container">
+            <div class="kiwi-settings-advanced-filter">
                 <input
                     v-model="filterString"
                     :placeholder="$t('settings_advanced_filter')"
@@ -11,59 +11,55 @@
                 <i v-if="!filterString" class="fa fa-search" aria-hidden="true" />
                 <i v-else class="fa fa-times" aria-hidden="true" @click="filterString = ''" />
             </div>
-            <table class="u-table kiwi-settings-advanced-table" cellspacing="0">
-                <tr v-if="filteredSettings.length === 0">
-                    <td class="kiwi-settings-advanced-noresult">
-                        {{ filterString }} - {{ $t('not_found') }}
-                    </td>
-                </tr>
-                <template v-else>
-                    <tr
-                        v-for="setting in filteredSettings"
-                        :key="setting.key"
-                        :class="{
-                            'kiwi-advanced-setting': !setting.modified,
-                            'kiwi-advanced-setting--modified': setting.modified,
-                        }"
-                    >
-                        <td><label :for="'setting-' + setting.key">{{ setting.key }}</label></td>
-                        <td v-if="setting.modified">
-                            <a class="u-link" @click="resetValue($event, setting.key)">
-                                {{ $t('settings_advanced_reset') }}
-                                <i class="fa fa-undo" style="margin-left: 10px;" />
-                            </a>
-                        </td>
-                        <td v-else />
-                        <td>
-                            <input
-                                v-if="setting.type === 'boolean'"
-                                :id="'setting-' + setting.key"
-                                :checked="setting.val"
-                                type="checkbox"
-                                @change="updateSetting($event, setting.key)"
-                            >
-                            <input
-                                v-else-if="setting.type === 'number'"
-                                :id="'setting-' + setting.key"
-                                :value="setting.val"
-                                class="u-input"
-                                type="number"
-                                @keydown.13="$event.target.blur()"
-                                @change="updateSetting($event, setting.key)"
-                                @blur="updateSetting($event, setting.key)"
-                            >
-                            <input
-                                v-else
-                                :id="'setting-' + setting.key"
-                                :value="setting.val"
-                                class="u-input"
-                                @keydown.13="$event.target.blur()"
-                                @blur="updateSetting($event, setting.key)"
-                            >
-                        </td>
-                    </tr>
+            <div v-if="filteredSettings.length === 0" class="kiwi-settings-advanced-empty">
+                "{{ filterString }}" {{ $t('not_found') }}
+            </div>
+            <div v-else class="kiwi-settings-advanced-table">
+                <template v-for="setting in filteredSettings">
+                    <div :key="'label-' + setting.key" class="kiwi-settings-advanced-key">
+                        <label
+                            :for="'setting-' + setting.key"
+                        >{{ setting.key }}</label>
+                    </div>
+                    <div :key="'reset-' + setting.key" class="kiwi-settings-advanced-modified">
+                        <a
+                            class="u-link"
+                            :class="{'kiwi-settings-advanced--modified': setting.modified}"
+                            @click="resetValue($event, setting.key)"
+                        >
+                            {{ $t('settings_advanced_reset') }}
+                            <i class="fa fa-undo" style="margin-left: 10px;" />
+                        </a>
+                    </div>
+                    <div :key="'value-' + setting.key" class="kiwi-settings-advanced-value">
+                        <input
+                            v-if="setting.type === 'boolean'"
+                            :id="'setting-' + setting.key"
+                            :checked="setting.val"
+                            type="checkbox"
+                            @change="updateSetting($event, setting.key)"
+                        >
+                        <input
+                            v-else-if="setting.type === 'number'"
+                            :id="'setting-' + setting.key"
+                            :value="setting.val"
+                            class="u-input"
+                            type="number"
+                            @keydown.13="$event.target.blur()"
+                            @change="updateSetting($event, setting.key)"
+                            @blur="updateSetting($event, setting.key)"
+                        >
+                        <input
+                            v-else
+                            :id="'setting-' + setting.key"
+                            :value="setting.val"
+                            class="u-input"
+                            @keydown.13="$event.target.blur()"
+                            @blur="updateSetting($event, setting.key)"
+                        >
+                    </div>
                 </template>
-            </table>
+            </div>
         </form>
     </div>
 </template>
@@ -137,80 +133,7 @@ export default {
 };
 </script>
 
-<style>
-
-.kiwi-settings-advanced {
-    width: 100%;
-}
-
-.kiwi-settings-advanced-table .u-input {
-    border-bottom: 2px solid red;
-    height: auto;
-    margin-top: 10px;
-}
-
-.kiwi-settings-advanced-table label {
-    margin: 0;
-}
-
-.kiwi-settings-advanced-table td {
-    height: 30px;
-}
-
-.kiwi-settings-advanced-table td:nth-child(2) {
-    min-width: 100px;
-}
-
-.kiwi-settings-advanced-table td:nth-child(3) {
-    min-width: 350px;
-}
-
-.kiwi-settings-advanced tr.kiwi-advanced-setting--modified {
-    font-weight: 900;
-}
-
-.kiwi-settings-advanced .u-table td .u-input {
-    height: 30px;
-}
-
-.kiwi-settings-advanced-filter {
-    border-bottom: 1px solid rgba(128, 128, 128, 0.5);
-    padding: 0 0 5px 0;
-}
-
-.kiwi-settings-advanced-filter-container {
-    position: relative;
-    display: inline-block;
-}
-
-.kiwi-settings-advanced-filter-container input::-ms-clear {
-    display: none;
-}
-
-.kiwi-settings-advanced-filter-container .fa-search,
-.kiwi-settings-advanced-filter-container .fa-times {
-    position: absolute;
-    top: 8px;
-    right: 10px;
-    z-index: 10;
-    cursor: default;
-}
-
-.kiwi-settings-advanced-filter-container .fa-times {
-    cursor: pointer;
-}
-
-.kiwi-settings-advanced-filter label {
-    font-weight: 600;
-}
-
-.kiwi-settings-advanced .u-form .kiwi-settings-advanced-filter .u-input {
-    display: inline-block;
-    border: 1px solid #000;
-    height: 40px;
-    padding: 0 10px;
-}
-
+<style lang="less">
 .kiwi-settings-advanced-notice {
     text-align: center;
     padding: 10px 0;
@@ -218,7 +141,34 @@ export default {
     font-weight: 900;
 }
 
-.kiwi-settings-advanced .kiwi-settings-advanced-noresult {
+.kiwi-settings-advanced-filter {
+    position: relative;
+    display: inline-block;
+
+    width: min(30%, 300px);
+
+    input {
+        width: 100%;
+    }
+
+    input::-ms-clear {
+        display: none;
+    }
+
+    .fa {
+        position: absolute;
+        top: 8px;
+        right: 10px;
+        z-index: 10;
+        cursor: default;
+
+        &.fa-times {
+            cursor: pointer;
+        }
+    }
+}
+
+.kiwi-settings-advanced-empty {
     width: 100%;
     margin: 50px 0 30px 0;
     text-align: center;
@@ -226,10 +176,74 @@ export default {
     font-size: 1em;
 }
 
-@media screen and (max-width: 600px) {
-    .kiwi-settings-advanced .u-form {
-        overflow-x: scroll;
+.kiwi-settings-advanced-table {
+    margin-top: 10px;
+    display: grid;
+    grid-template-columns: 1fr max-content 1fr;
+    grid-template-rows: auto;
+
+    > div {
+        padding-right: 20px;
+        height: 100%;
+    }
+
+    > div:nth-child(3n) {
+        padding-right: 10px;
+    }
+
+    .kiwi-settings-advanced-key,
+    .kiwi-settings-advanced-modified {
+        display: flex;
+        align-items: center;
+        padding-left: 10px;
+    }
+
+    .kiwi-settings-advanced-key label {
+        margin: 0;
+    }
+
+    .kiwi-settings-advanced-modified a {
+        visibility: hidden;
+    }
+
+    a.kiwi-settings-advanced--modified {
+        visibility: visible;
+    }
+
+    .kiwi-settings-advanced-value .u-input {
+        width: 100%;
+        padding: 1px 4px;
+        margin: 4px 0;
     }
 }
 
+@media screen and (max-width: 1100px) {
+    .kiwi-settings-advanced-table {
+        grid-template-columns: 1fr max-content;
+
+        .kiwi-settings-advanced-value {
+            grid-column: span 2;
+        }
+
+        > div {
+            box-sizing: border-box;
+            padding: 0 10px 8px 10px;
+        }
+
+        > div:nth-child(3n+1) {
+            padding-top: 8px;
+        }
+    }
+}
+
+@media screen and (max-width: 769px) {
+    .kiwi-settings-advanced-table {
+        display: flex;
+        flex-direction: column;
+
+        > div:nth-child(3n+3) {
+            padding-bottom: 8px;
+        }
+    }
+}
 </style>
