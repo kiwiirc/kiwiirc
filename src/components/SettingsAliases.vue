ae29ebe8940a4aebe443a5a7f73b5979d129cc1d
ItsOnlyBinary <ItsOnlyBinary@users.noreply.github.com>
2025-08-11 12:08:23 +0100

    Fix SettingsAliases html formatting (#1927)

--- src/components/SettingsAliases.vue
+++ src/components/SettingsAliases.vue
@@ -18,23 +18,26 @@
                 <p>
                     There are several variables that may be used to refer to the current environment
                     such as the active channel or active nick.
-                    <ul>
-                        <li><em>$server</em> The current network name</li>
-                        <li><em>$channel / $destination</em> The current channel / buffer name</li>
-                        <li><em>$nick</em> The current nick</li>
-                    </ul>
                 </p>
+                <ul>
+                    <li><em>$server</em> The current network name</li>
+                    <li><em>$channel / $destination</em> The current channel / buffer name</li>
+                    <li><em>$nick</em> The current nick</li>
+                </ul>
+
+                <p>You can also use variables to read input from the typed command.</p>
+                <ul>
+                    <li><em>$0</em> The command name</li>
+                    <li><em>$1</em> The first argument from the typed input</li>
+                    <li><em>$2</em> The second argument from the typed input</li>
+                    <li><em>$1+</em> From the first argument to the last argument</li>
+                </ul>
                 <p>
-                    You can also use variables to read input from the typed command. <br>
-                    <ul>
-                        <li><em>$0</em> The command name</li>
-                        <li><em>$1</em> The first argument from the typed input</li>
-                        <li><em>$2</em> The second argument from the typed input</li>
-                        <li><em>$1+</em> From the first argument to the last argument</li>
-                    </ul>
                     <b>Example 1:</b> <em>/greet /msg $1 Hello, $1!</em><br>
                     This creates an IRC command /greet that accepts one argument. Typing "/greet
-                    username" will execute "/msg username Hello, username!". <br>
+                    username" will execute "/msg username Hello, username!".
+                </p>
+                <p>
                     <b>Example 2:</b> <em>/ban /quote mode $channel +b $1+</em><br>
                     This creates an IRC command /ban that does a few things. Typing "/ban nick1
                     nick2" will execute "/quote mode #activechannel +b nick1 nick2". $channel is
@@ -43,25 +46,22 @@
                 </p>
 
                 <h4>Helper commands</h4>
-                <p>
-                    <ul>
-                        <li>
-                            <em>/echo</em><br>Sends a message to the active buffer without sending
-                            it to the IRC network. Eg, /echo Something happened
-                        </li>
-                        <li>
-                            <em>/lines</em><br>
-                            Similar to Mirc script, this lets you execute multiples lines of
-                            commands separated by a pipe, "|".<br>
-                            Example: "/lines /ban nick1 | /echo Banned user" would first execute the
-                            /ban command, and then the /echo command. This comes in handy with
-                            creating short aliases such as the common "/cycle" command that parts
-                            and re-joins the active channel: "/cycle /lines /part $channel | /join
-                            $channel".
-                        </li>
-                    </ul>
-
-                </p>
+                <ul>
+                    <li>
+                        <em>/echo</em><br>Sends a message to the active buffer without sending
+                        it to the IRC network. Eg, /echo Something happened
+                    </li>
+                    <li>
+                        <em>/lines</em><br>
+                        Similar to Mirc script, this lets you execute multiples lines of
+                        commands separated by a pipe, "|".<br>
+                        Example: "/lines /ban nick1 | /echo Banned user" would first execute the
+                        /ban command, and then the /echo command. This comes in handy with
+                        creating short aliases such as the common "/cycle" command that parts
+                        and re-joins the active channel: "/cycle /lines /part $channel | /join
+                        $channel".
+                    </li>
+                </ul>
             </div>
             <textarea v-model="aliasText" class="kiwi-settings-aliases-input" />
         </form>
